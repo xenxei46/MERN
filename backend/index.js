14226057714f9ca18cac6100275b998c7643ba4a
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2023-03-21 11:26:18 +0100

    native-todo

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 
-const api = process.env.API_URL
+const api = process.env.API_URL;
+// Middleware
+app.use(express.json());
+app.use(morgan('tiny'))
 
+app.get(`${api}/products`, (req, res) =>{
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_URLs',
+    }
+    res.send(product);
+})
 app.post(`${api}/products`, (req, res) =>{
-    // const product = {
-    //     id: 1,
-    //     name: 'hair dresser',
-    //     image: 'some_URL',
-    // }
-    // res.send(product);
     const newProduct = req.body;
     console.log(newProduct)
     res.send(newProduct)
+})
+
+mongoose.connect(proess.env.CONNECTION_STRING)
+.then(()=>{
+  console.log('Database Connection is ready...')
+})
+.catch((err)=>{
+  console.log(err)
 })
 
 app.listen(3000, ()=>{
