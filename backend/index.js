65bc81af41ceab7511381c3918b4996d444c19d7
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2023-03-22 13:41:35 +0100

    logging API Requests

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 // Middleware
 app.use(express.json());
+// app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
 app.get(`${api}/products`, (req, res) =>{
@@ -19,20 +21,26 @@ app.get(`${api}/products`, (req, res) =>{
     res.send(product);
 })
 app.post(`${api}/products`, (req, res) =>{
+    // const product = {
+    //     id: 1,
+    //     name: 'hair dresser',
+    //     image: 'some_URL',
+    // }
+    // res.send(product);
     const newProduct = req.body;
     console.log(newProduct)
     res.send(newProduct)
 })
 
-mongoose.connect(proess.env.CONNECTION_STRING)
-.then(()=>{
-  console.log('Database Connection is ready...')
-})
-.catch((err)=>{
-  console.log('not connected')
-})
+// mongoose.connect(process.env.CONNECTION_STRING)
+// .then(()=>{
+//   console.log('Database Connection is ready...')
+// })
+// .catch((err)=>{
+//   console.log('Database not connected')
+// })
 
-app.listen(3000, ()=>{
+app.listen(4000, ()=>{
   console.log(api);  
-  console.log('server is running http://localhost:3000');
+  console.log('server is running http://localhost:4000');
 })
