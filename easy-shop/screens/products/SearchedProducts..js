16e8bffe4bccac4616622ece5daec6e3ab7201e2
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2023-05-02 22:35:03 +0100

    searchcreated

---
+++ easy-shop/screens/products/SearchedProducts..js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions, Pressable, Image, FlatList} from 'react-native'
+import {Text , Button} from 'native-base';
+
+const data = require('../../assets/data/products.json');
+
+var { width } = Dimensions.get("window");
+
+const SearchedProduct = (props) => {
+    const { productsFiltered } = props;
+    return(
+        <View style={{width:width}}>
+            {productsFiltered.length > 0 ? (
+                <View>
+                    <FlatList
+                        data={data}
+                            onPress={() => {
+                                props.navigation.navigate("Product Detail", {item: item})
+                            }}
+                        renderItem={({item}) => 
+                        <View style={{display:'flex', flexDirection:'row', backgroundColor:'red',height:80 }} key={item.id}
+                        item={item}>
+                            <View style={{backgroundColor:'green', height:'100%', flex:1 }}>
+                                <Image
+                                    source={item.image ?  
+                                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'} style={{height:50, width:50}}
+                                />
+                            </View>
+                            <View style={{flex:4, height:'100%'}}> 
+                                <Text style={{fontWeight:'500'}}>{item.name}</Text>
+                                <Text style={{color:'grey', fontSize:10, }}>{item.description}</Text>
+                            </View>
+                        </View>
+                        
+                        // key={}
+                        }
+                        keyExtractor={item => item._id.$oid}  
+                    />
+                </View>
+                )
+             : (
+                <View style={styles.center}>
+                    <Text style={{ alignSelf:  'center' }}>
+                        No products match the selected criteria
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
+};
+
+export default SearchedProduct;
+
+const styles = StyleSheet.create({
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100
+    },
+    container: {
+    width: '200%',
+    height: '100%',
+    backgroundColor: 'grey'
+    }
+})
+
--- easy-shop/screens/products/productContainer.js
+++ easy-shop/screens/products/productContainer.js
@@ -1,32 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet,ActivityIndicator, FlatList } from 'react-native';
-// import {  Header, Icon, Item, Input, Container} from 'native-base';
-import { Button, Container, Heading, Icon, Item, Input } from 'native-base';
+import { View, StyleSheet,ActivityIndicator, FlatList, Pressable } from 'react-native';
+import { Button,Text, Container, HStack, Heading,IconButton, Icon, Item, Input } from 'native-base';
+import { MaterialIcons } from "@expo/vector-icons";
 import ProductList from './ProductList';
+import SearchedProduct from './SearchedProducts.';
 
 const data = require('../../assets/data/products.json');
 
 const ProductContainer = () => {
 
   const [ products, setProducts ] = useState([]);
+  const [ focus, setFocus ] = useState([]);
+  const [ productsFiltered, setProductsFiltered ] = useState([]);
 
   useEffect(() => {
-    setProducts(data); 
+    setProducts(data);
+    setProductsFiltered(data);
+    setFocus(false);
 
     return () => {
       setProducts([])
     }
   }, [])
      
+  const searchProduct = (text) => {
+    setProductsFiltered(
+      products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+    );
+  }; 
+
+    const openList = () => {
+      setFocus(true);
+    }
+
+    const onBlur = () => {
+      setFocus(false);
+    }
+
     return (
     <Container>
       <View>
-      {/* <Heading> */}
-          {/* A component library for the */}
-          <Text color="emerald.500"> React Ecosystem</Text>
-      {/* </Heading> */}
-      <View>
-          
+        <HStack px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+          <HStack alignItems="center" w="80%">
+          <Input onFocus={openList} onChangeText={(text) => searchProduct(text)}
+            placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="6" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<MaterialIcons name="search" />} />
+            
+            } 
+            />
+        
+            { focus == true ? 
+              <Pressable style={{backgroundColor:'red', height:20, width:20}} onPress={onBlur}>
+                <MaterialIcons name="close" /> 
+              </Pressable>
+            : null 
+            }
+
+          </HStack>
+        </HStack>
+        {focus == true ? (
+          <SearchedProduct
+             productsFiltered={productsFiltered}
+          />
+        ) : (
           <View style={{marginTop: 20}}>
           <FlatList
             // horizontal
@@ -38,9 +73,9 @@ const ProductContainer = () => {
             />}
             keyExtractor={item => item.name}
           />
-           <Button variant='outline' onPress={() => console.log('pressed')}>Click me</Button>
+           {/* <Button variant='outline' onPress={() => console.log('pressed')}>Click me</Button> */}
           </View>
-      </View> 
+        )}    
      </View> 
     </Container>      
     )
