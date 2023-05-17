a6ae383550df02acac03bd949f3ae18bd4b0b9f6
Wisdom John ikoi <wisdomjohnikoi@gmail.com>
2023-05-17 02:44:18 +0100

    search walkaround

--- easy-shop/screens/products/SearchedProducts.js
+++ easy-shop/screens/products/SearchedProducts.js
@@ -2,45 +2,62 @@ import React from 'react';
 import { View, StyleSheet, Dimensions, Pressable, Image,} from 'react-native'
 import {Text , FlatList, Avatar, Button} from 'native-base';
 
-const data = require('../../assets/data/products.json');
+const products = require('../../assets/data/products.json');
 
 var { width } = Dimensions.get("window");
 
-const SearchedProduct = ({productsFiltered}) => {
+const SearchedProduct = ({search}) => {
     // const { productsFiltered } = props;
     return(
         <View style={{width:width}}>
-            {productsFiltered.length > 0 ? (
-                <View>
-                    <FlatList
-                        data={data}
-                            onPress={() => {
-                                props.navigation.navigate("Product Detail", {item: item})
-                            }}
-                        renderItem={({item}) => 
-                        <View style={{flex:1, flexDirection:'row', backgroundColor:'red',height:80 }} key={item.id}
-                        item={item}>
-                            <View style={{backgroundColor:'green', height:'100%', flex:1 }}>
+            {search.length > 0 ? (
+            //     <View>
+            //         <FlatList
+            //             data={products}
+            //                 onPress={() => {
+            //                     props.navigation.navigate("Product Detail", {item: item})
+            //                 }}
+            //             renderItem={({item}) => 
+            //             <View style={{flex:1, flexDirection:'row', backgroundColor:'red',height:80 }} key={item.id}
+            //             item={item}>
+            //                 <View style={{backgroundColor:'green', height:'100%', flex:1 }}>
+            //                     <Avatar size="48px" source={{
+            //                                     uri: item.image ?  
+            //                                     item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'} 
+            //                      } />
+            //                     {/* <Image
+            //                         source={item.image ?  
+            //                             item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'} style={{height:50, width:50}}
+            //                     /> */}
+            //                 </View>
+            //                 <View style={{flex:4, height:'100%'}}> 
+            //                     <Text style={{fontWeight:'500'}}>{item.name}</Text>
+            //                     <Text style={{color:'grey', fontSize:10, }}>{item.description}</Text>
+            //                 </View>
+            //             </View>
+                        
+            //             // key={}
+            //             }
+            //             keyExtractor={item => item._id.$oid}  
+            //         />
+            // </View>
+            <View>
+       {products.filter((item) => item.name.toLowerCase().includes(search)
+        ).map((item) => (
+          <View key={item.id}>
+            <View style={{backgroundColor:'green', height:'100%', flex:1 }}>
                                 <Avatar size="48px" source={{
                                                 uri: item.image ?  
                                                 item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'} 
                                  } />
-                                {/* <Image
-                                    source={item.image ?  
-                                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'} style={{height:50, width:50}}
-                                /> */}
+                               
                             </View>
-                            <View style={{flex:4, height:'100%'}}> 
-                                <Text style={{fontWeight:'500'}}>{item.name}</Text>
-                                <Text style={{color:'grey', fontSize:10, }}>{item.description}</Text>
-                            </View>
-                        </View>
-                        
-                        // key={}
-                        }
-                        keyExtractor={item => item._id.$oid}  
-                    />
-                </View>
+            <Text>{item.name}</Text>
+            <Text>{item.description}</Text>
+          </View>
+
+        ))}
+      </View>
                 )
              : (
                 <View style={styles.center}>
--- easy-shop/screens/products/productContainer.js
+++ easy-shop/screens/products/productContainer.js
@@ -1,146 +1,167 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, StyleSheet,ActivityIndicator, FlatList, Pressable } from 'react-native';
-// import { Button,Text, Container, HStack, Heading,IconButton, Icon, Item, Input } from 'native-base';
-// import { MaterialIcons } from "@expo/vector-icons";
-// import ProductList from './ProductList';
-// import SearchedProduct from './SearchedProducts';
-// // import Banner from '../../Shared/Banner';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet,ActivityIndicator, FlatList, Pressable } from 'react-native';
+import { Button,Text, Container, HStack, Heading,IconButton, Icon, Item, Input } from 'native-base';
+import { MaterialIcons } from "@expo/vector-icons";
+import ProductList from './ProductList';
+import SearchedProduct from './SearchedProducts';
+// import Banner from '../../Shared/Banner';
 
-// const data = require('../../assets/data/products.json');
+const data = require('../../assets/data/products.json');
 
-// const ProductContainer = () => {
+const ProductContainer = () => {
 
-//   const [ products, setProducts ] = useState([]);
-//   const [ focus, setFocus ] = useState([]);
-//   const [ productsFiltered, setProductsFiltered ] = useState([]);
+  const [ products, setProducts ] = useState([]);
+  const [ focus, setFocus ] = useState([]);
+  // const [ productsFiltered, setProductsFiltered ] = useState('search products...');
+  const [search, setSearch] = React.useState('');
 
-//   useEffect(() => {
-//     setProducts(data);
-//     setProductsFiltered(data);
-//     setFocus(false);
-
-//     return () => {
-//       setProducts([])
-//     }
-//   }, [])
+  useEffect(() => {
+    setProducts(data);
+    // setProductsFiltered(data);
+    setSearch(search)
+    setFocus(false);
+    return () => {
+      setProducts([])
+    }
+  }, [])
      
-//   const searchProduct = (searchdata) => {
-//     setProductsFiltered(
-//       products.filter((item) => item.name.toLowerCase().includes(searchdata.toLowerCase()))
-//     );
-//   };
+  // const searchProduct = () => {
 
-//     const openList = () => {
-//       setFocus(true);
-//     }
+  //     products.filter((item) => item.name.toLowerCase().includes(productsFiltered))
+    
+  // };
 
-//     const onBlur = () => {
-//       setFocus(false);
-//     }
+  // const searchProduct = (text) => {
+  //   setSearch(
+  //     products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+  //   );
+  // };
 
-//     return (
-//     <Container>
-//       <View>
-//         <HStack px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
-//           <HStack alignItems="center" w="80%">
-//           <Input 
-//              onFocus={openList}
-//              onChangeText={(text) => searchProduct(text)} 
+    const openList = () => {
+      setFocus(true);
+    }
 
-//             placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="6" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<MaterialIcons name="search" />} />
-            
-//             } 
-//             />
-        
-//             { focus == true ? 
-//               <Pressable style={{backgroundColor:'red', height:20, width:20}} onPress={onBlur}>
-//                 <MaterialIcons name="close" /> 
-//               </Pressable>
-//             : null 
-//             }
+    const onBlur = () => {
+      setFocus(false);
+    }
 
-//           </HStack>
-//         </HStack>
-//         {focus == true ? (
-//           <SearchedProduct
-//              productsFiltered={productsFiltered}
-//           />
-//         ) : (
-//           <View style={{marginTop: 20}}>
-//             <View style={[styles.container]}>
-//               <View>
-//                 {/* <Banner/> */}
-//               </View>
-//                 <View>
-//                     <FlatList
-//                       // horizontal
-//                       numColumns={2}
-//                       data={products}
-//                       renderItem={({item}) => <ProductList
-//                       key={item.id}
-//                       item={item}
-//                       />}
-//                       keyExtractor={item => item.name}
-//                     />
-//                 </View>
-//             </View>
-//            {/* <Button variant='outline' onPress={() => console.log('pressed')}>Click me</Button> */}
-//           </View>
-//         )}    
-//      </View> 
-//     </Container>      
-//     )
-// }
-
-// export default ProductContainer;
-// const styles = StyleSheet.create({
-//   container: {
-//     flexWrap: "wrap",
-//     backgroundColor: "gainsboro",
-//   },
-// })
- 
-import { StyleSheet, Text, View,TextInput,SafeAreaView } from 'react-native'
-import React, {useState} from 'react'
-import Banner from '../../Shared/Banner';
-
-const products = require('../../assets/data/products.json')
-
-const productContainer = () => {
-  const [search, setSearch] = React.useState('search products...');
-  console.log(products.filter(item=>item.name.toLowerCase().includes(search)));
-  console.log(products)
-  return (
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={(e) => setSearch(e)}
-        value={search}
-      />
+    return (
+    <Container>
       <View>
-        {products.filter((item) => item.name.toLowerCase().includes(search)
-        ).map((item) => (
-          <View key={item.id}>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
-          </View>
+        <HStack px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
+          <HStack alignItems="center" w="80%">
+          <Input 
+             onFocus={openList}
+             onChangeText={(e) => setSearch(e)}
+            // onChangeText={(text) => searchProduct(text)}
+                     value={search}
 
-        ))}
-      </View>
-    </SafeAreaView>
-  )
+            placeholder="Search" variant="filled" width="100%" borderRadius="10" py="1" px="6" InputLeftElement={<Icon ml="2" size="4" color="gray.400" as={<MaterialIcons name="search" />} />
+            
+            } 
+            />
+        
+            { focus == true ? 
+              <Pressable style={{backgroundColor:'red', height:20, width:20}} onPress={onBlur}>
+                <MaterialIcons name="close" /> 
+              </Pressable>
+            : null 
+            }
+
+          </HStack>
+        </HStack>
+        {focus == true ? (
+          <SearchedProduct
+             search={search}
+          />
+          // <View>
+          //    {products.filter((item) => item.name.toLowerCase().includes(search)
+          //         ).map((item) => (
+          //           <View key={item.id}>
+          //             <Text>{item.name}</Text>
+          //             <Text>{item.description}</Text>
+          //           </View>
+          
+          //         ))}
+          // </View>
+          
+        ) : (
+          <View style={{marginTop: 20}}>
+            <View style={[styles.container]}>
+              <View>
+                {/* <Banner/> */}
+              </View>
+                <View>
+                    <FlatList
+                      // horizontal
+                      numColumns={2}
+                      data={products}
+                      renderItem={({item}) => <ProductList
+                      key={item.id}
+                      item={item}
+                      />}
+                      keyExtractor={item => item.name}
+                    />
+                </View>
+            </View>
+           {/* <Button variant='outline' onPress={() => console.log('pressed')}>Click me</Button> */}
+          </View>
+        )}    
+     </View> 
+    </Container>      
+    )
 }
 
-export default productContainer
-
+export default ProductContainer;
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  container: {
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
   },
-});
+})
+ 
+// import { StyleSheet, Text, View,TextInput,SafeAreaView } from 'react-native'
+// import React, {useState} from 'react'
+// import Banner from '../../Shared/Banner';
+
+// const products = require('../../assets/data/products.json')
+
+// const productContainer = () => {
+//   const [search, setSearch] = React.useState('search products...');
+//   console.log(products.filter(item=>item.name.toLowerCase().includes(search)));
+//   console.log(products)
+
+//   return (
+//     <SafeAreaView>
+//       <TextInput
+//         style={styles.input}
+//         onChangeText={(e) => setSearch(e)}
+//         value={search}
+//       />
+//       <View>
+//         {products.filter((item) => item.name.toLowerCase().includes(search)
+//         ).map((item) => (
+//           <View key={item.id}>
+//             <Text>{item.name}</Text>
+//             <Text>{item.description}</Text>
+//           </View>
+
+//         ))}
+//       </View>
+//     </SafeAreaView>
+//   )
+// }
+
+// export default productContainer
+
+// const styles = StyleSheet.create({
+//   input: {
+//     height: 40,
+//     margin: 12,
+//     borderWidth: 1,
+//     padding: 10,
+//   },
+// });
 
 
   
